ac8187393ef2e5afadc8e91680b0e85bce7db0e3
Hana00661 <eng.hana95@gmail.com>
2023-12-09 14:57:05 +0400

    try to  make single linked list

---
+++ single_linked_list.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *link;
+};
+
+void print_list(struct node *head) {
+  struct node *current = head;
+  while (current != NULL) {
+    printf("%d ", current->data);
+    current = current->link;
+  }
+  printf("\n");
+}
+
+int main() {
+    struct node *head = malloc(sizeof(struct node));
+    head->data = 45;
+    head->link = NULL;
+
+    struct node *current = malloc(sizeof(struct node));
+    current->data = 98;
+    current->link = NULL;
+    head->link = current;
+
+    current = malloc(sizeof(struct node));
+    current->data = 3;
+    current->link = NULL;
+    head->link->link = current;
+    print_list(head);
+    
+    return (0);
+}
